44e04a611afd82ce313715ddfd2c71bff2951472
Alina <alina@behrens-aachen.de>
2024-06-02 10:46:13 +0200

    completes information on history

--- src/Data/historyData.js
+++ src/Data/historyData.js
@@ -1,7 +1,7 @@
 export const historyData = [{
     title: "Alija",
     link: "/history/alija",
-    description: "Some very helpful short description", // TODO
+    description: "Jüdische Einwanderung nach Palästina",
     image: require("../Images/alija.jpg"),
     data: [{
         id: 0,
@@ -13,14 +13,14 @@ export const historyData = [{
 }, {
     title: "Britische Regierung",
     link: "/history/palaestina_unter_britischer_regisierung",
-    description: "",
-    image: "",
+    description: "Palästina unter britischer Regierung",
+    image: require("../Images/San-Remo-Conference.jpg"),
     data: [{
         id: 0,
         date: "2. Nov 1917",
         title: "Balfour-Deklaration",
         text: "Der britische Außenminister Arthur James Balfour verfasst einen Brief an den britischen Juden und Zionisten Lionel Walter Rothschild. In diesem sichert er ihm die Unterstützung der britischen Regierung bei der Gründung einer “nationalen Heimat für das jüdische Volk” in Palästina zu.",
-        image: require("../Images/alija.jpg")  // TODO
+        image: require("../Images/Balfour_declaration_unmarked.jpg")
     }, {
         id: 1,
         date: "1920",
@@ -43,12 +43,12 @@ export const historyData = [{
 }, {
     title: "Holocaust",
     link: "/history/holocaust",
-    description: "",
-    image: "",
+    description: "Verfolgung und Flucht",
+    image: require("../Images/Auschwitz.jpg"),
     data: [{
         id: 0,
         date: "1941-1945",
-        title: "Holucaust/Shoah",
+        title: "Holocaust/Shoah",
         text: "Unter der nationalsozialistischen Herrschaft wird während des Zweiten Weltkriegs die jüdische Bevölkerung Europas systematisch und mit industriellen Mitteln ermordet. Rund 6 Mio. jüdische Frauen, Männer und Kinder fallen den Nazi-Verbrechen zum Opfer.",
         image: require("../Images/Auschwitz.jpg")
     }, {
@@ -67,26 +67,102 @@ export const historyData = [{
 }, {
     title: "1. Teilungsplan",
     link: "/history/erster_teilungsplan",
-    description: "",
-    image: "",
+    description: "Aufteilung des Landes",
+    image: require("../Images/un-generalversammlung-teilungsplan-1947-1000x562.jpg.webp"),  // Foto: Presseamt der israelischen Regierung
     data: [{
         id: 0,
         date: "1947",
         title: "UN-Sonderkomission",
         text: "Überfordert mit der sich stetig verschlimmernden Situation in Palästina bittet die britische Besatzungsmacht die UN um Hilfe. Eine von der UN gegründete Sonderkommission erarbeitet daraufhin einen Teilungsplan, der im November 1947 von der UN-Generalversammlung bestätigt wird",
-        image: require("../Images/alija.jpg")  // TODO
+        image: require("../Images/un-generalversammlung-teilungsplan-1947-1000x562.jpg.webp")  // Foto: Presseamt der israelischen Regierung
     }, {
         id: 1,
         date: "1947",
         title: "Teilungsplan",
         text: "Der Plan sieht sowohl einen arabischen als auch einen jüdischen Staat in Palästina vor. In Jerusalem soll eine neutrale Zone entstehen. Während die jüdische Bevölkerung den Plan mit Jubel begrüßt, stößt er auf arabischer Seite auf Ärger: dort sieht man die jüdischen Gebiete als zu groß an.",
-        image: require("../Images/alija.jpg")  // TODO
-    },{
+        image: require("../Images/UN_Palestine_Partition_Versions_1947.jpg")
+    }, {
         id: 2,
         date: "9. Apr 1948",
         title: "Reaktionen",
         text: "Nach der Veröffentlichung des Teilungsplans eskaliert auf beiden Seiten die Gewalt.\n \n Eine israelische Untergrundorganisation ermordet 100 palästinensische Bewohner des Dorfes Deir Yassin. Beide Seiten verurteilten den Angriff. Palästinensische Kämpfer nahmen mit einem Angriff auf einen Hilfstransport Rache. Dabei starben 70 Ärzte, Krankenschwestern und Verwundete.",
-        image: require("../Images/alija.jpg")  // TODO
+        image: require("../Images/Jewish_militias_in_the_village_of_Deir_Yassin,_April_1948.jpg")
     },]
+}, {
+    title: "Staatsgründung Israels",
+    link: "/history/staatsgruendung_israel",
+    description: "Verkündung des Staats und Reaktionen",
+    image: require("../Images/Staatsgruendung.jpg"), // CC BY-SA 3.0 Datei:Flickr - Government Press Office (GPO) - David Ben Gurion reading the Declaration of Independence.jpg Erstellt: 14. Mai 1948 Hochgeladen: 16. Februar 2020 https://de.wikipedia.org/wiki/Israelische_Unabh%C3%A4ngigkeitserkl%C3%A4rung#/media/Datei:Flickr_-_Government_Press_Office_(GPO)_-_David_Ben_Gurion_reading_the_Declaration_of_Independence.jpg
+    data: [{
+        id: 0,
+        date: "14. Mai 1948",
+        title: "Staatsgründung Israels",
+        text: "Großbritannien beendet früher als von der UN vorgesehen das Mandat über Palästina. Der jüdische Nationalrat nutzt dies zur Verkündung des israelischen Staates. Diese wird innerhalb weniger Stunden von USA und UdSSR anerkannt.",
+        image: require("../Images/Staatsgruendung.jpg") // CC BY-SA 3.0 Datei:Flickr - Government Press Office (GPO) - David Ben Gurion reading the Declaration of Independence.jpg Erstellt: 14. Mai 1948 Hochgeladen: 16. Februar 2020 https://de.wikipedia.org/wiki/Israelische_Unabh%C3%A4ngigkeitserkl%C3%A4rung#/media/Datei:Flickr_-_Government_Press_Office_(GPO)_-_David_Ben_Gurion_reading_the_Declaration_of_Independence.jpg
+    }, {
+        id: 1,
+        date: "15. Mai 1948",
+        title: "Reaktionen der umliegenden Staaten",
+        text: "Noch in der Nacht nach der Verkündung des Staats greifen arabische Truppen aus Ägypten, Transjordanien, Syrien, Irak und Libanon Israel an. Den folgenden Krieg gewinnt Israel. Als dieser am 7. Jänner 1949 endet, hat es Israel geschafft, an Gebiet zu gewinnen.",
+        image: require("../Images/unabhaenigikeitskriegisrael-100_1280x720.jpeg")  // Foto: Keystone/AP/Israeli Government Press Office
+    }, {
+        id: 2,
+        date: "1948 - 1949",
+        title: "Israelisch-arabischer Krieg",
+        text: "Es folgt ein Krieg, der von Israel als Unabhängigkeitskrieg bezeichnet wird, während er von den umliegenden Staaten Katastrophe genannt wird.",
+        image: require("../Images/Haify.png")
+    }, {
+        id: 3,
+        date: "1949",
+        title: "Waffenstillstandsabkkommen",
+        text: "Der Krieg endete mit dem Waffenstillstandsabkommen von 1949 zwischen den Staaten Israel, Ägypten, Libanon, Syrien, Irak und Jordanien. Das nebenstehende Bild zeigt die vom UN-Teilungsplan vorgesehenden Regionen und das Ergebnis der Verteilung nach dem Krieg.",
+        image: require("../Images/partitionarmistice.gif")
+    }, {
+        id: 4,
+        date: "1967",
+        title: "Sechstagekrieg",
+        text: "Der Sechstagekrieg zwischen Israel und den arabischen Staaten Ägypten, Jordanien und Syrien dauert vom 5. bis zum 10. Juni 1967. Innerhalb dieser sechs Tage verdreifacht sich die Fläche Israels.",
+        image: require("../Images/sechstagekrieg.webp")  // Terry Fincher/ Getty Images
+    }, {
+        id: 5,
+        date: "ab 1967",
+        title: "Siedler in besetzten Gebieten",
+        text: "Nach Ende des Sechstagekrieges 1967 beginnt Israel Siedler in die besetzten Gebiete (Westjordanland, Gaza, Ost-Jerusalem, Golanhöhen) zu entsenden. Bewaffnete Soldaten/-innen bewachen die Siedlungen.\n \n Die UN sieht diese Siedlungen als völkerrechtswidrig. Auch der oberste Gerichtshof Israels hat sie mehrmals als verfassungswidrig eingestuft.",
+        image: require("../Images/Israelisettle_outpost.gif")  // Javier Zarracina/Vox
+    },
+    ]
+}, {
+    title: "Intifada & Hoffnung",
+    link: "/history/intifada_hoffnung",
+    description: "Aufstände und Hoffnung auf Frieden",
+    image: require("../Images/oslo-abkommen.jpg"),
+    data: [
+        {
+            id: 0,
+            date: "1987",
+            title: "1. Intifada",
+            text: "Die Erste Intifada, die 1987 beginnt, ist ein palästinensischer Aufstand gegen die israelische Besatzung im Westjordanland und Gazastreifen, ausgelöst durch tiefe Frustration und einen Verkehrsunfall, bei dem vier Palästinenser getötet werden. Der Aufstand zeichnet sich durch Massendemonstrationen, Streiks und gewaltsame Auseinandersetzungen aus, wobei sowohl die PLO als auch islamistische Gruppen beteiligt sind. Israel reagiert mit harten Sicherheitsmaßnahmen, was zu zahlreichen Toten und Verletzten auf beiden Seiten führt.",
+            image: require("../Images/Intifada_in_Gaza_Strip,_1987_V_Dan_Hadani_Archive.jpg")  // Efi Sharir / Photographer: Israel Press and Photo Agency (I.P.P.A.) / Dan Hadani collection, National Library of Israel / CC BY 4.0
+        },
+        {
+            id: 1,
+            date: "1993",
+            title: "Oslo-Abkommen",
+            text: "Der israelische Ministerpräsident Jitzchak Rabin und PLO-Führer Jassir Arafat einigen sich im Oslo-Abkommen auf fünf Jahre palästinensische Selbstverwaltung. Weitere Verhandlungen sollen die Krise endgültig lösen.",
+            image: require("../Images/oslo-abkommen.jpg")
+        }, {
+            id: 2,
+            date: "4. Nov 1995",
+            title: "Reaktionen auf das Abkommen",
+            text: "Auf beiden Seiten reagieren extreme Kräfte mit Wut auf das Oslo-Abkommen. Ein rechtsextremer Israelit erschießt bei einer Friedenskundgebung  Jitzchak Rabin. Nach dessen Tod geraten die Verhandlungen ins Stocken. Seither gestartete Versuche der Friedensverhandlung scheiterten allesamt.",
+            image: require("../Images/Assassination_of_Prime_Minister_Yitzhak_Rabin,_1995_XIII_Dan_Hadani_Archive.jpg")  // By Photographer: Israel Press and Photo Agency (I.P.P.A.) / Dan Hadani collection, National Library of Israel / CC BY 4.0, CC BY 4.0, https://commons.wikimedia.org/w/index.php?curid=111445478
+        }, {
+            id: 3,
+            date: "2000",
+            title: "2. Intifada",
+            text: "Die Zweite Intifada, auch Al-Aqsa-Intifada genannt, beginnt im September 2000 nach einem umstrittenen Besuch des damaligen israelischen Oppositionsführers Ariel Sharon auf dem Tempelberg in Jerusalem. Dieser Aufstand ist gekennzeichnet durch eine Eskalation der Gewalt, darunter Selbstmordanschläge, Schusswechsel und Militäraktionen, bei denen zahlreiche Palästinenser und Israelis getötet und verletzt werden. Israel reagiert mit massiven Militärschlägen, Abriegelungen und der Errichtung von Sperranlagen, was die humanitäre Lage in den palästinensischen Gebieten weiter verschärft.",
+            image: require("../Images/2nd_intifada.jpg")  // Nadav Ganot (נדב גנות) / IDF Spokesperson's Unit CC BY-SA 3.0
+        },
+    ]
 }
 ];
