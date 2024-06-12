a8892ff76d25616d42e1b0367f3bba3474eb24a0
Alina <alina@behrens-aachen.de>
2024-06-12 18:24:43 +0200

    new pictures in solutions

--- src/Data/solutionsData.js
+++ src/Data/solutionsData.js
@@ -5,27 +5,27 @@ export const solutionsData = [
         text: "Aus den drei Gebieten soll ein einheitlicher Staat entstehen. Dieser soll eine Demokratie sein. Arabischen und jüdischen Gruppen sollen die selben Rechte und Pﬂichten haben.",
         advantages: ["Keine Grenzkonﬂikte", "Jerusalem zugänglich"],
         disadvantages: ["Ablehnung beider Seiten", "Konﬂiktpotenzial"],
-        image: require("../Images/Balfour_declaration_unmarked.jpg")
+        image: require("../Images/Einstaat.jpeg")
     }, {
         id: 1,
         title: "Zweistaatenlösung",
         text: "Wie z.B. im UN-Teilungsplan oder den Oslo-Verhandlungen angestrebt, soll es einen israelischen und einen palästinensischen Staat geben. Diese sollen sich gegenseitig anerkennen. Manche Entwürfe sehen eine neutrale Zone in Jerusalem vor.",
         advantages: ["Beide Gruppen können nach eigenen Regeln leben"],
         disadvantages: ["Grenzziehung", "Jerusalem als Streitpunkt"],
-        image: require("../Images/Balfour_declaration_unmarked.jpg")
+        image: require("../Images/Zweistaaten.jpeg")
     }, {
         id: 2,
         title: "Dreistaatenlösung 1",
         text: "Die arabischen Gebiete im Gazastreifen und dem Westjordanland sollen umliegenden arabischen Staaten angegliedert werden: Gaza an Ägypten, das Westjordanland an Jordanien.",
         advantages: ["International anerkannte Staaten"],
         disadvantages: ["Grenzziehung", "Ägypten und Jordanien dagegen"],
-        image: require("../Images/Balfour_declaration_unmarked.jpg")
+        image: require("../Images/Dreistaaten_I.jpeg")
     }, {
         id: 3,
         title: "Dreistaatenlösung 2",
         text: "Der Gaza-Streifen und das Westjordanland werden jeweils einzelne, unabhängige Staaten. Diese beiden Staaten und Israel sollen sich gegenseitig anerkennen.",
         advantages: ["Beide Gruppen können nach eigenen Regeln leben"],
         disadvantages: ["Grenzziehung", "Trennung Palästinas", "Jerusalem als Streitpunkt"],
-        image: require("../Images/Balfour_declaration_unmarked.jpg")
+        image: require("../Images/Dreistaaten_II.jpeg")
     }
 ]
